f3c5c4f808defde5090e16a9db3495bf02086d72
mechlordx <sohlrich@ltu.edu>
2015-09-17 20:55:57 -0400

    plotter script

---
+++ IMSummer2015/Assets/plotter.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class plotter : MonoBehaviour {
+
+	public string button;
+	public float printLapse;
+	float currentLapse;
+	public float printTime;
+	float currentPrintTime;
+
+	// Use this for initialization
+	void Awake () {
+		currentPrintTime = 0f;
+		currentLapse = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (button))
+			currentPrintTime = printTime;
+
+		if(currentPrintTime>0f)
+		{
+			currentLapse += Time.deltaTime;
+			if(currentLapse>printLapse)
+			{
+				printBall();
+
+				currentLapse += -printLapse;
+				currentPrintTime += -printLapse;
+			}
+		}
+	}
+
+	void printBall()
+	{
+		GameObject.Instantiate (GameObject.CreatePrimitive (PrimitiveType.Sphere), transform.position, Quaternion.identity);
+	}
+}
