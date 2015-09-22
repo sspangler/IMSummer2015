1058f47c8e1e3a9d9f7e056559aa7c5af3801f1b
mechlordx <sohlrich@ltu.edu>
2015-09-22 19:15:22 -0400

    Push everything

---
+++ Physics/Assets/current.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class current : MonoBehaviour {
+
+	public float force;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += new Vector3 (0f, -force * Time.deltaTime, 0f);
+	}
+}
---
+++ Physics/Assets/move.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+
+	public float velocity;
+	public float xGoal = 84f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += transform.right * velocity * Time.deltaTime;
+		if (transform.position.x >= xGoal)
+			Debug.Break ();
+	}
+}
